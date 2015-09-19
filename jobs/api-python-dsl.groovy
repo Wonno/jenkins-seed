362afce99fa1ba9121bba348cd5cfae32df8a2fc
Dustin Collins <dustinrcollins@gmail.com>
2015-09-19 15:52:03 -0400

    api-python, testing

---
+++ jobs/api-python-dsl.groovy
@@ -0,0 +1,36 @@
+job('api-python-dsl') {
+  description('Test the Conjur Python client library')
+  label('docker && slave')
+  logRotator(30, -1, -1, 5)
+
+  scm {
+    git('git@github.com:conjurinc/api-python.git')
+  }
+
+  triggers {
+    githubPush()
+  }
+
+  wrappers {
+    preBuildCleanup()
+    colorizeOutput()
+    buildName('#${BUILD_NUMBER} ${GIT_BRANCH}')
+  }
+
+  steps {
+    shell('./jenkins.sh')
+  }
+
+  publishers {
+    archiveJunit('artifacts/pytest.xml')
+    cobertura('artifacts/coverage.xml')
+    publishHtml {
+      report('artifacts/htmlcov') {
+        reportName('Test Coverage')
+      }
+    }
+    violations(50) {
+      pylint(10, 999, 999, 'artifacts.pylint.out')
+    }
+  }
+}
