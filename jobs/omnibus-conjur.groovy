ce9f7d3b4d771a2f36c8ce7b5c04f1efa429e032
Dustin Collins <dustinrcollins@gmail.com>
2015-09-19 17:30:53 -0400

    omnibus-conjur-osx, ready to go

--- jobs/omnibus-conjur.groovy
+++ jobs/omnibus-conjur.groovy
@@ -1,4 +1,4 @@
-// Define the build flow to run following jobs in parallel
+// Build flow runs the following jobs in parallel
 buildFlowJob('omnibus-conjur') {
   description('Builds deb, rpm and pkg packages for the Conjur CLI in parallel.')
   logRotator(30, -1, -1, 5)
@@ -50,5 +50,46 @@ buildFlowJob('omnibus-conjur') {
     publishers {
       archiveArtifacts('built/*')
     }
+  }
+}
+
+// OSX job is a little different
+job('omnibus-conjur-osx') {
+  description('Builds the Conjur CLI package for osx')
+  label('osx && slave')
+  logRotator(30, -1, -1, 5)
+
+  parameters {
+    stringParam('BUILD_VERSION', 'LATEST', 'Version of the CLI to build')
+  }
+
+  scm {
+    git('git@github.com:conjurinc/omnibus-conjur.git')
+  }
+
+  wrappers {
+    preBuildCleanup()
+    colorizeOutput()
+    rvm('2.1.5@omnibus-conjur-osx')
+    buildName('#${BUILD_NUMBER} ${GIT_BRANCH}')
+  }
+
+  steps {
+    shell('sudo rm -rf /opt/conjur/*')
+    shell('''
+      gem install bundler
+      bundle install --binstubs
+    '''.stripIndent())
+    shell('''
+      if [ \"\${BUILD_VERSION}\" == \"LATEST\" ]; then
+        export BUILD_VERSION=\$(gem query -r -n conjur-cli | grep -oE \"[0-9]{1,2}.[0-9]{1,2}.[0-9]{1,2}\")
+      fi
+      security unlock-keychain -p "10693930"
+      bundle exec bin/omnibus build conjur
+    '''.stripIndent())
+  }
+
+  publishers {
+    archiveArtifacts('pkg/*')
   }
 }
