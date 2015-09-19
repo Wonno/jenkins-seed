aa62295a14944e45f9af10ed4fb2d73ad8050ee6
Dustin Collins <dustinrcollins@gmail.com>
2015-09-19 17:02:38 -0400

    Add build version param to omnibus-conjur

--- jobs/omnibus-conjur.groovy
+++ jobs/omnibus-conjur.groovy
@@ -2,6 +2,10 @@ buildFlowJob('omnibus-conjur') {
   description('Builds deb, rpm and pkg packages for the Conjur CLI in parallel.')
   logRotator(30, -1, -1, 5)
 
+  parameters {
+    stringParam('BUILD_VERSION', 'LATEST', 'Version of the CLI to build')
+  }
+
   buildFlow('''
     parallel (
       { build("omnibus-conjur-centos", BUILD_VERSION: params["BUILD_VERSION"]) },
