bdf056ac99ee43c383c8e667e1ff745e92d1d415
Dustin Collins <dustinrcollins@gmail.com>
2015-09-29 17:07:42 -0400

    simplify Docker Appliance pipeline view title

--- views/docker_appliance_pipeline.groovy
+++ views/docker_appliance_pipeline.groovy
@@ -1,7 +1,7 @@
 // Conjur appliance build pipeline view
 
 buildPipelineView('Docker Appliance Pipeline') {
-  title('Conjur Docker Appliance build pipeline')
+  title('Docker Appliance')
   filterExecutors()
   displayedBuilds(5)
   selectedJob('appliance_docker_build')
