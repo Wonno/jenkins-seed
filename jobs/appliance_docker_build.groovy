e8de0afec916a561c04bfef9572c5c11b16d3193
Dustin Collins <dustinrcollins@gmail.com>
2015-09-29 17:03:16 -0400

    Pass the build tag from docker build job to downstream test jobs

--- jobs/appliance_docker_build.groovy
+++ jobs/appliance_docker_build.groovy
@@ -24,6 +24,7 @@ def job = job('appliance_docker_build') {
         parameters {
           currentBuild()
           gitRevision()
+          predefinedProp('APPLIANCE_IMAGE_TAG', '$BUILD_TAG')
         }
       }
     }
