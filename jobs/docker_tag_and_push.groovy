85da31faad4f737fd495a68e5a68b820014dda98
Dustin Collins <dustinrcollins@gmail.com>
2015-09-30 11:06:12 -0400

    docker_tag_and_push job

---
+++ jobs/docker_tag_and_push.groovy
@@ -0,0 +1,20 @@
+import utilities.Config
+
+def job = job('docker_tag_and_push') {
+  description('Adds a new tag to an existing Docker image and pushes it to the registry')
+
+  parameters {
+    stringParam('IMAGE_NAME', '', 'Docker image name')
+    stringParam('IMAGE_TAG_CURRENT', '', 'Existing image tag')
+    stringParam('IMAGE_TAG_NEW', '', 'New image tag')
+  }
+
+  steps {
+    shell('''
+      docker tag $IMAGE_NAME:$IMAGE_TAG_CURRENT $IMAGE_NAME:$IMAGE_TAG_NEW
+      docker push $IMAGE_NAME
+    '''.stripIndent())
+  }
+}
+
+Config.applyCommonConfig(job)
