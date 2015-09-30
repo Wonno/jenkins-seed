8562dfb100187684b745f7872d4e387c69dad41c
Dustin Collins <dustinrcollins@gmail.com>
2015-09-30 11:40:37 -0400

    Conditionally create an AMI after tag/push

--- jobs/docker_tag_and_push.groovy
+++ jobs/docker_tag_and_push.groovy
@@ -2,6 +2,7 @@ import utilities.Config
 
 def job = job('docker_tag_and_push') {
   description('Adds a new tag to an existing Docker image and pushes it to the registry')
+  concurrentBuild()
 
   parameters {
     stringParam('IMAGE_NAME', '', 'Docker image name')
@@ -15,6 +16,27 @@ def job = job('docker_tag_and_push') {
       docker tag -f $IMAGE_NAME:$IMAGE_TAG_CURRENT $IMAGE_NAME:$IMAGE_TAG_NEW
       docker push $IMAGE_NAME:$IMAGE_TAG_NEW
     '''.stripIndent())
+
+    conditionalSteps {
+      condition {
+        booleanCondition('$RELEASE_AMI')
+      }
+      steps {
+        downstreamParameterized {
+          trigger('appliance-docker-ami') {
+            block {
+                buildStepFailure('FAILURE')
+                failure ('FAILURE')
+                unstable('UNSTABLE')
+            }
+            parameters {
+                predefinedProp('APPLIANCE_IMAGE', '$IMAGE_NAME')
+                predefinedProp('APPLIANCE_IMAGE_TAG', '$IMAGE_TAG_NEW')
+            }
+          }
+        }
+      }
+    }
   }
 }
 
