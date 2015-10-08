2d154aae743cf4804a86655a23dd9e879ac02126
Dustin Collins <dustinrcollins@gmail.com>
2015-10-08 14:13:04 -0400

    Use a map for downstream test jobs

--- jobs/conjur_asset_ui.groovy
+++ jobs/conjur_asset_ui.groovy
@@ -5,15 +5,34 @@ def mainJobName = 'conjur_asset_ui'
 def repoUrl = 'git@github.com:conjurinc/conjur-asset-ui.git'
 
 def testJobs = [
-  "${mainJobName}_test_frontend",
-  "${mainJobName}_test_backend",
-  "${mainJobName}_test_acceptance"
+  [name: "${mainJobName}_test_frontend", script: './test_frontend.sh'],
+  [name: "${mainJobName}_test_backend", script: './test_backend.sh'],
+  [name: "${mainJobName}_test_acceptance", script: './test_acceptance.sh']
 ]
+// Save this in the format downstream expects
+def testJobNames = ['name'].collect{testJobs[it]}[0].join(',')
 
 def mainJob = job(mainJobName) {
+  concurrentBuild()
+
+  properties {
+    promotions {
+      promotion {
+        name("Tests passed")
+        icon('star-blue')
+        conditions {
+          downstream(false, testJobNames)
+        }
+        actions {
+          //TODO: push to dev Docker registry here
+        }
+      }
+    }
+  }
+
   publishers {
     downstreamParameterized {
-      trigger(testJobs.join(',')) {
+      trigger(testJobNames) {
         condition('SUCCESS')
         parameters {
           currentBuild()
@@ -27,8 +46,12 @@ Config.addGitRepo(mainJob, repoUrl)
 Config.applyCommonConfig(mainJob)
 
 testJobs.each { testJob ->
-  def j = job(testJob) {
+  def j = job(testJob['name']) {
+    concurrentBuild()
 
+    steps {
+      shell(testJob['script'])
+    }
   }
   Config.addGitRepo(j, repoUrl, false)
   Config.applyCommonConfig(j)
