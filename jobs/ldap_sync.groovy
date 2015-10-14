747b6a0487d8f7bee2b3aa07814cf208128d13c0
Dustin Collins <dustinrcollins@gmail.com>
2015-10-14 14:53:11 -0400

    Update ldap-sync job for deb packaging

--- jobs/ldap_sync.groovy
+++ jobs/ldap_sync.groovy
@@ -1,65 +1,16 @@
-// Defines ldap-sync build and acceptance jobs
 use(conjur.Conventions) {
-  def buildJob = job('ldap-sync-build') {
-    description('''
-      Builds and pushes ldap-sync to Docker registry
-
-      <a href="https://github.com/conjurinc/ldap-sync/blob/master/README.md">README</a>
-    '''.stripIndent())
+  def job = job('ldap-sync') {
+    description('Build and test ldap-sync')
 
     steps {
-      shell('make build/base build/test build/push')
+      shell('./jenkins.sh')
     }
 
     publishers {
-      downstreamParameterized {
-        trigger('ldap-sync-acceptance') {
-          condition('SUCCESS')
-          parameters {
-            currentBuild()
-            gitRevision()
-            predefinedProp('TEST_TAG', 'conjurinc/acceptance-ldap-sync:build_${BUILD_NUMBER}')
-          }
-        }
-      }
-    }
-
-    properties {
-      promotions {
-        promotion {
-          name("Release to production")
-          icon("star-green")
-          conditions {
-            // def downstream(Boolean evenIfUnstable, String jobs)
-            downstream(false, 'ldap-sync-acceptance')
-          }
-        }
-      }
+      archiveArtifacts('*.deb')
+      archiveJunit('spec/reports/*.xml, features/reports/*.xml')
     }
   }
-  buildJob.applyCommonConfig()
-  buildJob.addGitRepo('git@github.com:conjurinc/ldap-sync.git')
-
-  def testJob = job('ldap-sync-acceptance') {
-    description('Runs acceptance tests against ldap-sync image built by ldap-sync-build')
-
-    parameters {
-      stringParam('TEST_TAG', '', 'Tagged appliance image coming from ldap-sync-build')
-    }
-
-    steps {
-      shell("""
-        export PATH=/opt/conjur/bin:$PATH   # somehow it's not done automatically
-
-        conjur env run -c acceptance.conjurenv -- make acceptance/results
-      """.stripIndent())
-    }
-
-    publishers {
-      archiveJunit('test/cukes_report/*/*,test/spec_report/*/*')
-      archiveArtifacts('test/*')
-    }
-  }
-  testJob.applyCommonConfig()
-  testJob.addGitRepo('git@github.com:conjurinc/ldap-sync.git', false)
+  job.applyCommonConfig()
+  job.addGitRepo('git@github.com:conjurinc/ldap-sync.git')
 }
