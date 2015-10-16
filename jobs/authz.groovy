108c05b22e0eeed22100f540f7705e3b6ac07652
Dustin Collins <dustinrcollins@gmail.com>
2015-10-16 16:57:43 -0400

    DSL authz job

---
+++ jobs/authz.groovy
@@ -0,0 +1,24 @@
+use(conjur.Conventions) {
+  def job = job('authz') {
+    description('Test the Conjur authz core service')
+
+    wrappers {
+      rvm('2.0.0@conjur-authz')
+    }
+
+    steps {
+      shell('''
+        set -e
+        gem install -N bundler
+        bundle install --without "production appliance"
+        ./jenkins.sh
+      '''.stripIndent())
+    }
+
+    publishers {
+      archiveJunit('spec/reports/*.xml, features/reports/*.xml')
+    }
+  }
+  job.applyCommonConfig()
+  job.addGitRepo('git@github.com:conjurinc/authz.git')
+}
