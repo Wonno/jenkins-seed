35fb5e479e68fffa0cf9d615ec1349260114c630
Dustin Collins <dustinrcollins@gmail.com>
2015-10-19 11:37:22 -0400

    [#105884586] Create release_dockerhub job

---
+++ jobs/release_dockerhub.groovy
@@ -0,0 +1,20 @@
+use(conjur.Conventions) {
+  def job = job('release_dockerhub') {
+    description('Publish an image to DockerHub through releasebot/pubbit')
+
+    parameters {
+      stringParam('DOCKER_IMAGE', '', 'The name of the image to promote.')
+      stringParam('DOCKER_TAG', '', 'The tag to apply to the image.')
+    }
+
+    steps {
+      shell('''
+        env LOCAL_REGISTRY=$CONJUR_DOCKER_REGISTRY LOCAL_REGISTRY_USER=conjurinc \
+        builder/bin/promote_docker_image $DOCKER_IMAGE $DOCKER_TAG
+      '''.stripIndent())
+    }
+  }
+  job.applyCommonConfig()
+  job.addGitRepo('git@github.com:conjurinc/pubbit.git', false)
+  job.setBuildName('#${BUILD_NUMBER} ${ENV,var="DOCKER_IMAGE"}:${ENV,var="DOCKER_TAG"}')
+}
