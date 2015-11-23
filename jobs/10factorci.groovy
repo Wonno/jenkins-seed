ff0a157b9218d1d1f69aa03b28e6bb4069923952
Dustin Collins <dustinrcollins@gmail.com>
2015-11-23 15:46:13 -0500

    [#105711614] 10factorci jenkins job

---
+++ jobs/10factorci.groovy
@@ -0,0 +1,20 @@
+use(conjur.Conventions) {
+  def job = job('10factorci') {
+    steps {
+      downstreamParameterized {
+        trigger('release_heroku') {
+          block {
+            buildStepFailure('FAILURE')
+            failure('FAILURE')
+            unstable('UNSTABLE')
+          }
+          parameters {
+            predefinedProp('APP_NAME', 'tenfactorci-conjur')
+          }
+        }
+      }
+    }
+  }
+  job.applyCommonConfig()
+  job.addGitRepo('git@github.com:conjurinc/10factorci.git', true, '*/master')
+}
