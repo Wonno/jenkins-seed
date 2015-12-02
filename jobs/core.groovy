9315171f5f79be46a8f081d37cf557b937670fea
Dustin Collins <dustinrcollins@gmail.com>
2015-12-02 14:35:41 -0500

    Run jenkins.sh for core

--- jobs/core.groovy
+++ jobs/core.groovy
@@ -2,19 +2,8 @@ use(conjur.Conventions) {
   def job = job('core') {
     description('Test the Conjur core ...core service')
 
-    wrappers {
-      rvm('2.0.0@conjur-core')
-    }
-
     steps {
-      shell('''
-        bundle install
-        SKIP_AUDIT_TESTS=yes DATABASE_SCHEMA=core-ci
-        CONJURRC=/var/lib/jenkins/conjurops/.conjurrc \
-        bundle exec conjur env:run \
-        --yaml "{ conjur_admin_password: !var build-0.1.0/conjur-ci/admin-password , conjurrc:  /var/lib/jenkins/.conjurrc , conjur_account: ci, conjurapi_log: stderr, restclient_log: stderr }" \
-        -- bundle exec rake jenkins
-      '''.stripIndent())
+      shell('./jenkins.sh')
     }
 
     publishers {
