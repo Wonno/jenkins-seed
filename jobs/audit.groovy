b783bfec259ff12cbbf4aa6d6efe62538f769631
Kevin Gilpin <kgilpin@gmail.com>
2015-12-10 10:44:02 -0500

    Create audit.groovy

---
+++ jobs/audit.groovy
@@ -0,0 +1,15 @@
+use(conjur.Conventions) {
+  def job = job('audit') {
+    description('Test the Conjur audit core service')
+
+    steps {
+      shell('./jenkins.sh')
+    }
+
+    publishers {
+      archiveJunit('spec/reports/*.xml, features/reports/*.xml')
+    }
+  }
+  job.applyCommonConfig()
+  job.addGitRepo('git@github.com:conjurinc/audit.git')
+}
