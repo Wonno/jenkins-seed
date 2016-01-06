c9e0a1a546ecb041a2e00ba70cc66c4fd1e284fd
Dustin Collins <dustinrcollins@gmail.com>
2016-01-06 10:33:53 -0500

    Publish evoke deb on master if tests pass

--- jobs/evoke.groovy
+++ jobs/evoke.groovy
@@ -5,13 +5,23 @@ use(conjur.Conventions) {
     steps {
       shell('./jenkins.sh')
     }
-    
+
     publishers {
       archiveArtifacts('*.deb')
       archiveJunit('spec/reports/*.xml, features/reports/*.xml')
+      postBuildScripts {
+        steps {
+          shell('''
+            if [ "\$GIT_BRANCH" != "origin/master" ]; then
+              make publish
+            fi
+          '''.stripIndent())
+        }
+        onlyIfBuildSucceeds()
+      }
     }
   }
-  
+
   job.applyCommonConfig()
   job.addGitRepo('git@github.com:conjurinc/evoke.git')
 }
