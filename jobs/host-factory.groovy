bf191f441435ccc7a612a99aa9b6b5b75b55ee50
Kevin Gilpin <kgilpin@gmail.com>
2016-01-10 11:43:31 -0500

    Remove rvm

--- jobs/host-factory.groovy
+++ jobs/host-factory.groovy
@@ -2,10 +2,6 @@ use(conjur.Conventions) {
   def job = job('host-factory') {
     description('host-factory - generate host identities for servers and VMs')
 
-    wrappers {
-      rvm('2.0.0@host-factory')
-    }
-    
     steps {
       shell('./jenkins.sh')
     }
