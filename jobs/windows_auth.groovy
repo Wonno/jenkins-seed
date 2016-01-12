b264cd08bce44b04e00bfd382e1105547fc28b7d
Rafał Rzepecki <divided.mind@gmail.com>
2016-01-12 16:13:39 +0100

    Add windows-auth job

---
+++ jobs/windows_auth.groovy
@@ -0,0 +1,22 @@
+def repoUrl = 'git@github.com:conjurinc/windows-auth.git'
+def projectName = 'windows-auth'
+
+use(conjur.Conventions) {
+  def job = job(projectName) {
+    description("""
+    <p>Build windows authorization binaries.
+    <p>
+    <a href="https://github.com/conjurinc/windows-auth/">
+      https://github.com/conjurinc/windows-auth/
+    </a>
+  """.stripIndent())
+    steps {
+      shell("./jenkins.sh")
+    }
+    publishers {
+      archiveArtifacts('bin/*')
+    }
+  }
+  job.applyCommonConfig()
+  job.addGitRepo(repoUrl, true)
+}
