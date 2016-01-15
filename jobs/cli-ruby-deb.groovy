6abea8b0f4d4ab74783f11c672ed850705cb7d60
Kevin Gilpin <kgilpin@gmail.com>
2016-01-15 13:16:15 -0500

    Wrap in RVM

--- jobs/cli-ruby-deb.groovy
+++ jobs/cli-ruby-deb.groovy
@@ -2,6 +2,10 @@ use(conjur.Conventions) {
   def job = job('cli-ruby-deb') {
     description('Builds a non-Omnibus deb for the Ruby CLI')
 
+    wrappers {
+      rvm('2.0.0@cli-ruby-deb')
+    }
+
     steps {
       shell('./build-deb.sh')
     }
