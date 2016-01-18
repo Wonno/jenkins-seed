9ee59399e4a9b299f2180b0b64718ab647cf43c3
Kevin Gilpin <kgilpin@gmail.com>
2016-01-18 18:00:56 -0500

    Create pubkeys.groovy

---
+++ jobs/pubkeys.groovy
@@ -0,0 +1,23 @@
+use(conjur.Conventions) {
+  def job = job('expiration') {
+    description('''
+      pubkeys - Manage and distribute public keys
+    '''.stripIndent())
+
+    steps {
+      shell('./jenkins.sh')
+    }
+
+    publishers {
+      archiveJunit('features/reports/*.xml')
+      postBuildScripts {
+          steps {
+              shell('./publish.sh')
+          }
+          onlyIfBuildSucceeds(true)
+      }
+    }
+  }
+  job.applyCommonConfig()
+  job.addGitRepo('git@github.com:conjurinc/pubkeys.git')
+}
