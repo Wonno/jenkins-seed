2ac37ebf9f517e12198a3bd3f4acd5b573155a33
Alan Potter <alanjpotter@gmail.com>
2016-01-20 13:01:35 -0500

    add job for conjur-cookbook

---
+++ jobs/conjur_cookbook.groovy
@@ -0,0 +1,22 @@
+// TODO: 
+//   * add publish step (uses ./publish.sh)
+//   * add postbuild steps
+//     * scan console log, foodcritic parser
+//     * report violations (10 999 999 for each 
+
+use(conjur.Conventions) {
+  def job = job('conjur-cookbook') {
+    description('Lints and tests the <a href="https://github.com/conjur-cookbooks/conjur">conjur</a> cookbook')
+
+    steps {
+      shell('./jenkins.sh')
+    }
+
+    publishers {
+      archiveArtifacts('features/reports/*.xml, spec/reports/*.xml')
+    }
+  }
+
+  job.applyCommonConfig()
+  job.addGitRepo('git@github.com:conjur-cookbooks/conjur.git')
+}
