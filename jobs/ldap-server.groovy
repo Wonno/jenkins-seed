055cda92583e68c75ad3abe0fb3afe1ee675fc2d
Kurt Stutsman <kstutsman@conjur.net>
2016-01-22 09:18:57 -0600

    add job for ldap-server

---
+++ jobs/ldap-server.groovy
@@ -0,0 +1,28 @@
+use(conjur.Conventions) {
+  def job = job('ldap-server') {
+    description('Test the Conjur ldap-server service')
+
+    steps {
+      shell('./jenkins.sh')
+    }
+
+    publishers {
+      archiveJunit('reports/*.xml)
+      plotBuildData {
+        plot('Performance', 'performance.csv') {
+          style('line')
+          yAxis('requests per second')
+          csvFile('performance.csv')
+        }
+      }
+      postBuildScripts {
+          steps {
+              shell('./publish.sh')
+          }
+          onlyIfBuildSucceeds(true)
+      }
+    }
+  }
+  job.applyCommonConfig()
+  job.addGitRepo('git@github.com:conjurinc/ldap-server.git')
+}
