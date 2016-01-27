07e9bb4407fea754311dbd0a6cfcf8d58bb4eace
Dustin Collins <dustinrcollins@gmail.com>
2016-01-27 14:50:35 -0500

    Use conventions to apply common config to template job

--- jobs/_template_conjur_service.groovy
+++ jobs/_template_conjur_service.groovy
@@ -2,21 +2,23 @@ def templatesFolder = 'templates'
 
 folder(templatesFolder)
 
-def templateJob = job("${templatesFolder}/conjur_service") {
-  description("Template job for Conjur services")
-  steps {
-    shell('./jenkins.sh')
-  }
+use(conjur.Conventions) {
+  def templateJob = job("${templatesFolder}/conjur_service") {
+    description("Template job for Conjur services")
+    steps {
+      shell('./jenkins.sh')
+    }
 
-  publishers {
-    archiveArtifacts('*.deb')
-    archiveJunit('spec/reports/*.xml, features/reports/*.xml')
-    postBuildScripts {
-        steps {
-            shell('./publish.sh')
-        }
-        onlyIfBuildSucceeds(true)
+    publishers {
+      archiveArtifacts('*.deb')
+      archiveJunit('spec/reports/*.xml, features/reports/*.xml')
+      postBuildScripts {
+          steps {
+              shell('./publish.sh')
+          }
+          onlyIfBuildSucceeds(true)
+      }
     }
   }
+  templateJob.applyCommonConfig()
 }
-templateJob.applyCommonConfig()
