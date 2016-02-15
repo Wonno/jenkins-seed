c94f2658bac78a43d6c48f1500ff4c40089ea804
Dustin Collins <dustinrcollins@gmail.com>
2016-02-15 16:17:54 -0500

    Push nginx package when build succeeds

--- jobs/nginx.groovy
+++ jobs/nginx.groovy
@@ -2,12 +2,22 @@ use(conjur.Conventions) {
   def job = job('nginx') {
     description('Build custom nginx package for installation on the appliance')
 
+    parameters {
+      stringParam('DISTRIBUTION', '5.0', 'apt distribution to push package to')
+    }
+
     steps {
       shell('./jenkins.sh')
     }
 
     publishers {
       archiveArtifacts('*.deb')
+      postBuildScripts {
+        steps {
+          shell('debify publish --component stable $DISTRIBUTION nginx')
+        }
+        onlyIfBuildSucceeds(true)
+      }
     }
   }
 
