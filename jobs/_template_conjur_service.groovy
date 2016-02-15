858fbde490ed91f8013848363328812439156c8f
Dustin Collins <dustinrcollins@gmail.com>
2016-02-15 15:02:20 -0500

    Add publishing step to appliance service jobs

--- jobs/_template_conjur_service.groovy
+++ jobs/_template_conjur_service.groovy
@@ -7,6 +7,28 @@ use(conjur.Conventions) {
     description("Template job for Conjur services")
     steps {
       shell('./jenkins.sh')
+      shell('''
+        #!/bin/bash -ex
+
+        # Publishes a Debian package via debify
+
+        export DEBUG=true
+        export GLI_DEBUG=true
+
+        PACKAGE_NAME=$JOB_NAME
+        DISTRIBUTION=$(cat VERSION_APPLIANCE)
+        COMPONENT=$(echo $GIT_BRANCH | sed "s/^origin\///" | tr '/' '.')
+
+        echo "Publishing $PACKAGE_NAME to distribution '$DISTRIBUTION', component '$COMPONENT'"
+
+        debify publish \
+        --component $COMPONENT \
+        $DISTRIBUTION \
+        $PACKAGE_NAME
+
+        touch "DISTRIBUTION=\$DISTRIBUTION"
+        touch "COMPONENT=\$COMPONENT"
+      '''.stripIndent())
     }
 
     publishers {
