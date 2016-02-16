0e91c95adef6e304174c3021115c84e5cf1cde8c
Dustin Collins <dustinrcollins@gmail.com>
2016-02-16 09:52:32 -0500

    Publish CLI deb after tests pass

--- jobs/cli-ruby-deb.groovy
+++ jobs/cli-ruby-deb.groovy
@@ -2,6 +2,10 @@ use(conjur.Conventions) {
   def job = job('cli-ruby-deb') {
     description('Builds a non-Omnibus deb for the Ruby CLI')
 
+    parameters {
+      stringParam('DISTRIBUTION', '5.0', 'apt distribution to push package to')
+    }
+
     wrappers {
       rvm('2.0.0@cli-ruby-deb')
     }
@@ -12,6 +16,12 @@ use(conjur.Conventions) {
 
     publishers {
       archiveJunit('spec/reports/*.xml, features/reports/*.xml, acceptance-features/reports/*.xml')
+      postBuildScripts {
+        steps {
+          shell('./publish.sh $DISTRIBUTION')
+        }
+        onlyIfBuildSucceeds(true)
+      }
     }
   }
   job.applyCommonConfig()
