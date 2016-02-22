678c4defe04d0566830f6fb42819049ca053173e
Kurt Stutsman <kstutsman@conjur.net>
2016-02-22 09:33:32 -0600

    Publish to conjurtools apt repo

--- jobs/ldap_sync.groovy
+++ jobs/ldap_sync.groovy
@@ -2,6 +2,10 @@ use(conjur.Conventions) {
   def job = job('ldap-sync') {
     description('Build and test ldap-sync')
 
+    parameters {
+      stringParam('DISTRIBUTION', 'conjurtools', 'apt distribution to push package to')
+    }
+
     steps {
       shell('./jenkins.sh')
     }
@@ -10,7 +14,32 @@ use(conjur.Conventions) {
       archiveArtifacts('*.deb')
       archiveJunit('spec/reports/*.xml, features/reports/*.xml')
     }
+
+    properties {
+      promotions {
+        promotion {
+          name("Publish to apt stable")
+          icon("star-gold")
+          conditions {
+            manual('')
+          }
+          actions {
+            copyArtifacts('$PROMOTED_JOB_NAME') {
+              includePatterns(artifacts)
+              buildSelector {
+                buildNumber('$PROMOTED_NUMBER')
+              }
+            }
+            environmentVariables {
+              propertiesFile('env.properties')
+            }
+            shell('debify publish --component stable $DISTRIBUTION ldap-sync')
+          }
+        }
+      }
+    }
   }
+
   job.applyCommonConfig()
   job.addGitRepo('git@github.com:conjurinc/ldap-sync.git')
 }
