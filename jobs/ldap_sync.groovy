bcfbb6905965710fa5ddaece01f967e9f37acd8c
Kurt Stutsman <kstutsman@conjur.net>
2016-02-22 13:10:10 -0600

    Use publish.sh method of publishing

--- jobs/ldap_sync.groovy
+++ jobs/ldap_sync.groovy
@@ -15,29 +15,11 @@ use(conjur.Conventions) {
     publishers {
       archiveArtifacts('*.deb')
       archiveJunit('spec/reports/*.xml, features/reports/*.xml')
-    }
-
-    properties {
-      promotions {
-        promotion {
-          name("Publish to apt stable")
-          icon("star-gold")
-          conditions {
-            manual('')
-          }
-          actions {
-            copyArtifacts('$PROMOTED_JOB_NAME') {
-              includePatterns(artifacts)
-              buildSelector {
-                buildNumber('$PROMOTED_NUMBER')
-              }
-            }
-            environmentVariables {
-              propertiesFile('env.properties')
-            }
-            shell('debify publish --component stable $DISTRIBUTION ldap-sync')
-          }
+      postBuildScripts {
+        steps {
+          shell('./publish.sh $DISTRIBUTION')
         }
+        onlyIfBuildSucceeds(true)
       }
     }
   }
