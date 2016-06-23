8a0ca4374f3951219257e1da1174c74fbcdbbe27
Dustin Collins <dustinrcollins@gmail.com>
2016-06-23 12:53:09 -0400

    Regex jobs for failing builds view

--- views/failing_builds.groovy
+++ views/failing_builds.groovy
@@ -1,12 +1,17 @@
 listView('Failing Builds') {
   description('Builds that are currently unstable or worse')
 
+  jobs {
+    regex('/.*/')
+  }
+
   jobFilters {
     status {
       status(Status.UNSTABLE)
       status(Status.FAILED)
     }
   }
+
   columns {
     status()
     weather()
