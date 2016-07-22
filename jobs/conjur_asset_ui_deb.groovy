ea05a8af2306316e347043a746129d72e232cee9
Rafał Rzepecki <divided.mind@gmail.com>
2016-07-22 11:39:18 -0400

    Add job for conjur-asset-ui-deb

---
+++ jobs/conjur_asset_ui_deb.groovy
@@ -0,0 +1,18 @@
+use(conjur.Conventions) {
+  def job = job('conjur-asset-ui-deb') {
+    description('Build debs for Conjur UI')
+
+    steps {
+      shell('./jenkins.sh')
+    }
+
+    publishers {
+      archiveArtifacts {
+        pattern('app/*.deb')
+        pattern('server/*.deb')
+      }
+    }
+  }
+  job.applyCommonConfig()
+  job.addGitRepo("git@github.com:conjurinc/conjur-asset-ui.git", true, 'work/on-appliance')
+}
