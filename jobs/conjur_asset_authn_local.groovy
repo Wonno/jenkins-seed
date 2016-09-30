28b623969723c4639db2918821d5212f7352b535
Dustin Collins <dustinrcollins@gmail.com>
2016-09-30 15:44:41 -0400

    fix syntax typo

--- jobs/conjur_asset_authn_local.groovy
+++ jobs/conjur_asset_authn_local.groovy
@@ -1,40 +1,41 @@
 use(conjur.Conventions) {
-    def job = job('conjur-asset-authn-local') {
-        description('Client gem for authn-local')
+  def job = job('conjur-asset-authn-local') {
+    description('Client gem for authn-local')
 
-        steps {
-            shell('./jenkins.sh')
-        }
+    steps {
+      shell('./jenkins.sh')
+    }
 
-        publishers {
-            archiveJunit('features/reports/*.xml')
-        }
+    publishers {
+      archiveJunit('features/reports/*.xml')
+    }
 
-        properties {
-          promotions {
-            promotion {
-              name('Publish to rubygems')
-              conditions {
-                manual('')
-              }
-              actions {
-                downstreamParameterized {
-                  trigger('release-rubygems') {
-                    block {
-                      buildStepFailure('FAILURE')
-                      failure('FAILURE')
-                      unstable('UNSTABLE')
-                    }
-                    parameters {
-                      predefinedProp('GEM_NAME', 'conjur-asset-authn-local')
-                    }
-                  }
+    properties {
+      promotions {
+        promotion {
+          name('Publish to rubygems')
+          conditions {
+            manual('')
+          }
+          actions {
+            downstreamParameterized {
+              trigger('release-rubygems') {
+                block {
+                  buildStepFailure('FAILURE')
+                  failure('FAILURE')
+                  unstable('UNSTABLE')
+                }
+                parameters {
+                  predefinedProp('GEM_NAME', 'conjur-asset-authn-local')
                 }
               }
             }
           }
         }
+      }
+    }
+  }
 
-    job.applyCommonConfig()
-    job.addGitRepo('git@github.com:conjurinc/conjur-asset-authn-local.git')
+  job.applyCommonConfig()
+  job.addGitRepo('git@github.com:conjurinc/conjur-asset-authn-local.git')
 }
