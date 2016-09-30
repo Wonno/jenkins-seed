250b9036f03a85daf699f73d8f07e7e3543f224d
Dustin Collins <dustinrcollins@gmail.com>
2016-09-30 15:50:51 -0400

    Fix promotion for conjur_rack

--- jobs/conjur_rack.groovy
+++ jobs/conjur_rack.groovy
@@ -13,15 +13,21 @@ use(conjur.Conventions) {
     properties {
       promotions {
         promotion {
-          name("Publish to RubyGems")
-          icon("star-gold")
+          name('Publish to rubygems')
           conditions {
             manual('')
           }
           actions {
             downstreamParameterized {
-              trigger("release-rubygems", "SUCCESS", false, ["buildStepFailure": "FAILURE","failure":"FAILURE","unstable":"UNSTABLE"]) {
-                predefinedProp("GEM_NAME", "conjur-rack")
+              trigger('release-rubygems') {
+                block {
+                  buildStepFailure('FAILURE')
+                  failure('FAILURE')
+                  unstable('UNSTABLE')
+                }
+                parameters {
+                  predefinedProp('GEM_NAME', 'conjur-rack')
+                }
               }
             }
           }
