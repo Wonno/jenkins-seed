d13a557a912d74a01abc73d73c64181dc136ec99
Dustin Collins <dustinrcollins@gmail.com>
2016-09-30 15:46:54 -0400

    Fix promotion for conjur_asset_policy

--- jobs/conjur_asset_policy.groovy
+++ jobs/conjur_asset_policy.groovy
@@ -1,35 +1,41 @@
 use(conjur.Conventions) {
-    def job = job('conjur-asset-policy') {
-        description('Test the Conjur policy plugin')
+  def job = job('conjur-asset-policy') {
+    description('Test the Conjur policy plugin')
 
-        steps {
-            shell('./jenkins.sh')
-        }
-
-        publishers {
-            archiveJunit('spec/reports/*.xml, features/reports/*.xml')
-        }
-
-        properties {
-            promotions {
-                promotion {
-                    name("Release to Rubygems")
-                    icon("star-gold")
-                    conditions {
-                        manual('')
-                    }
-                    actions {
-                        downstreamParameterized {
-                            trigger("release-rubygems", "SUCCESS", false, ["buildStepFailure": "FAILURE","failure":"FAILURE","unstable":"UNSTABLE"]) {
-                                predefinedProp("GEM_NAME","conjur-asset-policy")
-                            }
-                        }
-                    }
-                }
-            }
-        }
+    steps {
+      shell('./jenkins.sh')
     }
 
-    job.applyCommonConfig()
-    job.addGitRepo('git@github.com:conjurinc/conjur-asset-policy.git')
+    publishers {
+      archiveJunit('spec/reports/*.xml, features/reports/*.xml')
+    }
+
+    properties {
+      promotions {
+        promotion {
+          name('Publish to rubygems')
+          conditions {
+            manual('')
+          }
+          actions {
+            downstreamParameterized {
+              trigger('release-rubygems') {
+                block {
+                  buildStepFailure('FAILURE')
+                  failure('FAILURE')
+                  unstable('UNSTABLE')
+                }
+                parameters {
+                  predefinedProp('GEM_NAME', 'conjur-asset-policy')
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+
+  job.applyCommonConfig()
+  job.addGitRepo('git@github.com:conjurinc/conjur-asset-policy.git')
 }
