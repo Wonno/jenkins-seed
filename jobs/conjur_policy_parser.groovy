370cb5fea94a8f9b8e523c1c94e96d6e3685c618
Dustin Collins <dustinrcollins@gmail.com>
2016-09-30 15:49:41 -0400

    Fix promotion for conjur_policy_parser

--- jobs/conjur_policy_parser.groovy
+++ jobs/conjur_policy_parser.groovy
@@ -1,35 +1,41 @@
 use(conjur.Conventions) {
-    def job = job('conjur-policy-parser') {
-        description('Test the policy parser')
+  def job = job('conjur-policy-parser') {
+    description('Test the policy parser')
 
-        steps {
-            shell('./jenkins.sh')
-        }
-
-        publishers {
-            archiveJunit('spec/reports/*.xml, features/reports/*.xml')
-        }
-
-        properties {
-            promotions {
-                promotion {
-                    name("Release to Rubygems")
-                    icon("star-gold")
-                    conditions {
-                        manual('')
-                    }
-                    actions {
-                        downstreamParameterized {
-                            trigger("release-rubygems", "SUCCESS", false, ["buildStepFailure": "FAILURE","failure":"FAILURE","unstable":"UNSTABLE"]) {
-                                predefinedProp("GEM_NAME","conjur-policy-parser")
-                            }
-                        }
-                    }
-                }
-            }
-        }
+    steps {
+      shell('./jenkins.sh')
     }
 
-    job.applyCommonConfig()
-    job.addGitRepo('git@github.com:conjurinc/conjur-policy-parser.git')
+    publishers {
+      archiveJunit('spec/reports/*.xml, features/reports/*.xml')
+    }
+
+    properties {
+      promotions {
+        promotion {
+          name('Publish to rubygems')
+          conditions {
+            manual('')
+          }
+          actions {
+            downstreamParameterized {
+              trigger('release-rubygems') {
+                block {
+                  buildStepFailure('FAILURE')
+                  failure('FAILURE')
+                  unstable('UNSTABLE')
+                }
+                parameters {
+                  predefinedProp('GEM_NAME', 'conjur-policy-parser')
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+
+  job.applyCommonConfig()
+  job.addGitRepo('git@github.com:conjurinc/conjur-policy-parser.git')
 }
