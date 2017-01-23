fd04265dde6c2077ff06183021b459194177173a
Dustin Collins <dustinrcollins@gmail.com>
2017-01-23 14:55:12 -0600

    Keep builds/artifacts for 60 days, breathing room

--- lib/conjur/Conventions.groovy
+++ lib/conjur/Conventions.groovy
@@ -16,7 +16,7 @@ class Conventions {
 
     job.with {
       label('executor')
-      logRotator(30, -1, 30, -1)  // keep builds/artifacts for 30 days
+      logRotator(60, -1, 60, -1)  // keep builds/artifacts for 60 days
 
       concurrentBuild()
       throttleConcurrentBuilds {
