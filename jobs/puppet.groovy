1fdc61d974be6e7f2ac962fcd1ff43a2b3aa39d3
Dustin Collins <dustinrcollins@gmail.com>
2017-01-30 15:20:31 -0600

    Create Jenkins job for Puppet module [CON-3840]

---
+++ jobs/puppet.groovy
@@ -0,0 +1,16 @@
+use(conjur.Conventions) {
+  def job = job('puppet') {
+    description('Official Conjur Puppet module')
+
+    steps {
+      shell('./jenkins.sh')
+    }
+
+    publishers {
+      archiveJunit('rspec.xml')
+    }
+  }
+
+  job.addGitRepo('git@github.com:conjur/puppet.git')
+  job.applyCommonConfig()
+}
