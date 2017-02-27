713fafc8583a1bc6b2a5d827db0c953e60dfd82b
Dustin Collins <dustinrcollins@gmail.com>
2017-02-27 14:10:00 -0600

    Build artifact-url on new executors

--- jobs/artifact_url.groovy
+++ jobs/artifact_url.groovy
@@ -6,6 +6,6 @@ use(conjur.Conventions) {
       shell('docker build -t artifact-url .')
     }
   }
-  job.applyCommonConfig()
+  job.applyCommonConfig(label: 'executor-v2')
   job.addGitRepo('git@github.com:conjurinc/artifact-url.git')
 }
