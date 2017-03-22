828b893d4fb09c8b94abc632bec16b1bd8205a41
Dustin Collins <dustinrcollins@gmail.com>
2017-03-22 11:33:27 -0500

    Run the conjur-cookbook pipeline weekly against master branch

--- jobs/cron_jobs.groovy
+++ jobs/cron_jobs.groovy
@@ -15,6 +15,10 @@ def service_branches  = [
   [name: 'v4.7',   cron: cron_weekly]
 ]
 
+def other_cron_jobs = [
+  [name: 'conjur-cookbook', cron: cron_weekly]
+]
+
 // Create a __cron folder to hold jobs
 def folderName = '__cron'
 folder(folderName) {
@@ -64,6 +68,28 @@ use(conjur.Conventions) {
         }
       }
       j.applyCommonConfig()
+    }
+  }
+}
+
+// Other cron jobs, various jobs that need to run on a schedule
+use(conjur.Conventions) {
+  other_cron_jobs.each { cron_job ->
+    def j = job("${folderName}/${cron_job.name}") {
+      description("Triggers job ${cron_job.name} to run regularly on master branch")
+      triggers {
+        cron(cron_job.cron)
+      }
+      steps {
+        downstreamParameterized {
+          trigger(cron_job.name) {
+            parameters {
+              predefinedProp('BRANCH', 'master')
+            }
+          }
+        }
+      }
+    j.applyCommonConfig()
     }
   }
 }
