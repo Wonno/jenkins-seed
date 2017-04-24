cbc12adf5498eb9473273f5ec84edc622b31e94c
Dustin Collins <dustinrcollins@gmail.com>
2017-04-24 17:19:16 -0500

    Build rpm if available

--- jobs/uml_image.groovy
+++ jobs/uml_image.groovy
@@ -1,25 +1,27 @@
 use(conjur.Conventions) {
   def job = job('uml-image') {
     description('Convert Docker image to a SquashFS image for UML runtime.')
-    
+
     parameters {
       stringParam('IMAGE', null, 'Tag of the docker image to convert, eg. registry.example/library/plan9:latest')
     }
-    
+
     steps {
       shell('docker pull $IMAGE')
       shell('./mkimage.sh $IMAGE')
+      shell('[[ -f mkrpm.sh ]] && ./mkrpm.sh')
     }
-    
+
     publishers {
       archiveArtifacts {
         pattern('*.sqsh')
+        pattern('*.rpm')
+        fingerprint()
       }
     }
   }
-  
+
   job.addGitRepo('git@github.com:conjurinc/appliance-uml.git', false)
   job.applyCommonConfig(cleanup: false)
   job.setBuildName('$IMAGE')
 }
-
