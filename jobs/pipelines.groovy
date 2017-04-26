bf4512d8415490e6235a887d23f2f089e2bf795c
Dustin Collins <dustinrcollins@gmail.com>
2017-04-26 16:40:10 -0500

    Add build discard, health icon to pipelines

--- jobs/pipelines.groovy
+++ jobs/pipelines.groovy
@@ -4,12 +4,23 @@ def pipelines = [
 
 pipelines.each { pipeline ->
   multibranchPipelineJob(pipeline.name) {
+
     branchSources {
       github {
         repoOwner(pipeline.owner)
         repository(pipeline.name)
         scanCredentialsId('conjur-jenkins')
       }
+    }
+
+    orphanedItemStrategy {
+      discardOldItems {
+        numToKeep(20)
+      }
+    }
+
+    configure { project ->
+      project / 'healthMetrics' / 'com.cloudbees.hudson.plugins.folder.health.WorstChildHealthMetric'
     }
   }
 }
