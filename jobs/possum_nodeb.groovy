9fba08384da92e84e5205e51d5dff6f99c909f4a
Kevin Gilpin <kgilpin@gmail.com>
2017-05-05 14:44:56 -0400

    Push to heroku after successfull build on master

--- jobs/possum_nodeb.groovy
+++ jobs/possum_nodeb.groovy
@@ -1,11 +1,33 @@
 use(conjur.Conventions) {
   def job = job('possum_nodeb') {
-    description('Test Possum')
+    description('Possum server')
 
     steps {
       shell('./jenkins.sh')
-    }
-
+      conditionalSteps {
+        condition {
+          stringsMatch('${GIT_BRANCH}', 'origin/master', false)
+        }
+        runner('Run')
+        steps {
+          downstreamParameterized {
+            trigger('release-heroku') {
+              block {
+                buildStepFailure('FAILURE')
+                failure('FAILURE')
+                unstable('UNSTABLE')
+              }
+              parameters {
+                predefinedProp('APP_NAME', 'possum-ci-conjur')
+                currentBuild()
+                gitRevision()
+              }
+            }
+          }
+        }
+      }
+    }    
+    
     publishers {
       archiveJunit('spec/reports/*.xml, features/reports/*.xml, cucumber/api/features/reports/*.xml, cucumber/policy/features/reports/*.xml')
     }
