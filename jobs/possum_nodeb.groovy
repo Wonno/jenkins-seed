b49426f8ac231987bc10e78c0c388d50a78ae98c
Alan Potter <alanjpotter@gmail.com>
2017-06-08 18:31:25 -0400

    don't expect test results

--- jobs/possum_nodeb.groovy
+++ jobs/possum_nodeb.groovy
@@ -28,10 +28,6 @@ use(conjur.Conventions) {
       }
     }    
     
-    publishers {
-      archiveJunit('spec/reports/*.xml, features/reports/*.xml, cucumber/api/features/reports/*.xml, cucumber/policy/features/reports/*.xml')
-    }
-
     wrappers {
       // note: necessary because of broken permissions from the
       // docker build process; remove after fixing that
