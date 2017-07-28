bbc3d4059e120f10b0b2144c1d51e2c04624844e
Dustin Collins <dustinrcollins@gmail.com>
2017-07-28 11:58:41 -0500

    Revert "Make sure pipelined services show up in appliance services view"
    This reverts commit 72b8ea91d06899b204a80fbad40f6109a0dccb65.

--- views/appliance_services.groovy
+++ views/appliance_services.groovy
@@ -1,14 +1,13 @@
 import conjur.Appliance
 
-def pipelines = ['authn']
-def dsl_jobs = Appliance.getServices().each { service -> name(service) }
-
 listView('Appliance Services') {
   description('Services built into the Conjur appliance')
   filterBuildQueue()
 
   jobs {
-    pipelines + dsl_jobs
+    Appliance.getServices().each { service ->
+      name(service)
+    }
   }
   columns {
     status()
