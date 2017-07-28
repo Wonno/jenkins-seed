72b8ea91d06899b204a80fbad40f6109a0dccb65
Dustin Collins <dustinrcollins@gmail.com>
2017-07-28 11:56:35 -0500

    Make sure pipelined services show up in appliance services view

--- views/appliance_services.groovy
+++ views/appliance_services.groovy
@@ -1,13 +1,14 @@
 import conjur.Appliance
 
+def pipelines = ['authn']
+def dsl_jobs = Appliance.getServices().each { service -> name(service) }
+
 listView('Appliance Services') {
   description('Services built into the Conjur appliance')
   filterBuildQueue()
 
   jobs {
-    Appliance.getServices().each { service ->
-      name(service)
-    }
+    pipelines + dsl_jobs
   }
   columns {
     status()
