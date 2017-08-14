082ee5476ea938b099bc2b0515f3a3aa8078316c
Dustin Collins <dustinrcollins@gmail.com>
2017-08-14 16:24:21 -0500

    Fix pipeline naming after repo move

--- jobs/pipelines.groovy
+++ jobs/pipelines.groovy
@@ -1,24 +1,40 @@
 def pipelines = [
-  [repo: 'conjurinc/jenkins-seed'],
-  [repo: 'conjurinc/appliance-uml'],
-  [repo: 'conjurinc/conjur-ui'],
-  [repo: 'conjurinc/apidocs'],
-  [repo: 'conjurinc/appliance'],
-  [repo: 'conjurinc/appliance-docker-ami', buildName: 'appliance-docker-ami-pipeline'],
-  [repo: 'cyberark/conjur'],
-  [repo: 'conjurinc/possum-cpanel'],
+  // 'cyberark' org
+  [repo: 'cyberark/conjur'],  // Conjur 5 ->
+
+  // - CLIs
+  [repo: 'cyberark/conjur-cli'],
   [repo: 'cyberark/summon'],
   [repo: 'cyberark/summon-conjur'],
-  [repo: 'cyberark/api-go', buildName: 'api-go-pipeline'],
-  [repo: 'cyberark/api-ruby', buildName: 'api-ruby-pipeline'],
-  [repo: 'cyberark/cli-ruby', buildName: 'cli-ruby-pipeline'],
+
+  // - Integrations
+  [repo: 'cyberark/conjur-puppet'],
+  [repo: 'cyberark/ansible-role-conjur'],
+
+  // - API clients
+  [repo: 'cyberark/conjur-api-dotnet'],
+  [repo: 'cyberark/conjur-api-go'],
+  [repo: 'cyberark/conjur-api-java'],
+  [repo: 'cyberark/conjur-api-ruby'],
+
+  // 'conjur' org
+
+  // 'conjurinc' org
+  [repo: 'conjurinc/appliance'],  // Docker distribution
+  [repo: 'conjurinc/conjur-ui'],  // Integrated UI
+  [repo: 'conjurinc/appliance-uml'], // UML/RPM distribution
+  [repo: 'conjurinc/appliance-docker-ami', buildName: 'appliance-docker-ami-pipeline'],
+  [repo: 'conjurinc/jenkins-seed'],
+  [repo: 'conjurinc/apidocs'],
+  [repo: 'conjurinc/possum-cpanel'],
   [repo: 'conjurinc/nginx'],
-  [repo: 'conjur/puppet'],
   [repo: 'conjurinc/github_hooks'],
   [repo: 'conjurinc/java-example'],
   [repo: 'conjurinc/authn'],
-  [repo: 'cyberark/api-java'],
   [repo: 'conjurinc/conjurops-policies']
+
+  // 'conjur-cookbooks' org
+
 ]
 
 pipelines.each { pipeline ->
