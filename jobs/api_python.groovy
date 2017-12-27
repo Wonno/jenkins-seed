6d3cde4c8b0ec65e48ba06fecb29b6ce4d343441
Dustin Collins <dustin.collins@cyberark.com>
2017-12-27 15:58:40 -0600

    Run api-python on v2 executors

--- jobs/api_python.groovy
+++ jobs/api_python.groovy
@@ -24,6 +24,6 @@ use(conjur.Conventions) {
       }
     }
   }
-  job.applyCommonConfig()
+  job.applyCommonConfig(label: 'executor-v2')
   job.addGitRepo('git@github.com:conjurinc/api-python.git')
 }
