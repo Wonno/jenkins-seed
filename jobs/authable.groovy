c0bef45843d576f6fe1c3fb09fdd48c3418aec16
Dustin Collins <dustin.collins@cyberark.com>
2018-01-02 12:16:22 -0600

    Tag authable build as disabled, for now

--- jobs/authable.groovy
+++ jobs/authable.groovy
@@ -1,5 +1,7 @@
 use(conjur.Conventions) {
   def job = job('authable') {
+    disabled()
+
     description('''
       SCHEDULED BUILD: weekly (to detect regressions against 3rd-party gems)
       <br>
@@ -13,14 +15,17 @@ use(conjur.Conventions) {
     }
 
     steps {
-      shell('bash -c "source ~/.rvm/scripts/rvm && rvm use --install --create 2.0.0@conjur-authable && export > rvm.env"')
-      shell('source rvm.env && ./jenkins.sh')
+      shell('echo "This build is disabled until it can be containerized to run on v2 executors."')
+      shell('exit 1')
+
+      // shell('bash -c "source ~/.rvm/scripts/rvm && rvm use --install --create 2.0.0@conjur-authable && export > rvm.env"')
+      // shell('source rvm.env && ./jenkins.sh')
     }
 
     publishers {
       archiveJunit('spec/reports/*.xml, features/reports/*.xml')
     }
   }
-  job.applyCommonConfig()
+  job.applyCommonConfig(label: 'executor-v2')
   job.addGitRepo('git@github.com:conjurinc/authable.git')
 }
