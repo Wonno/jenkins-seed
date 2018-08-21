9717e8231ecab9fdfe3ea8d6016b27d2be6c82a2
Gary Moon <garymoon@users.noreply.github.com>
2018-08-21 15:36:15 -0400

    Remove Windows installer

--- jobs/pipelines.groovy
+++ jobs/pipelines.groovy
@@ -11,7 +11,6 @@ def pipelines = [
   [repo: 'cyberark/summon-conjur'],
   [repo: 'cyberark/summon-file'],
   [repo: 'conjurinc/summon-s3'],  // TODO: move this to cyberark org
-  [repo: 'conjurinc/summon-windows-installer'],
   [repo: 'cyberark/summon-aws-secrets'],
 
   // - Integrations
